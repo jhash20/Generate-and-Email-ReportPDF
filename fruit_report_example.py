feeb5be571857f921117d6b0e82b650cef225c40
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-17 21:38:52 -0700

    Create fruit_report_example.py
    Created an example python script that will use the two modules, report_pdf_generator and report_pdf_emailer, to create a report and send it by email

---
+++ fruit_report_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import emails
+import os
+import reports
+
+table_data = [
+  ['Name', 'Amount', 'Value'],
+  ['elderberries', 10, 0.45],
+  ['figs', 5, 3],
+  ['apples', 4, 2.75],
+  ['durians', 1, 25],
+  ['bananas', 5, 1.99],
+  ['cherries', 23, 5.80],
+  ['grapes', 13, 2.48],
+  ['kiwis', 4, 0.49]]
+report_pdf_generator.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit." table_data)
+sender = "automation@example.com"
+receiver = "{}@example.com".format(os.environ.get('USER'))
+subject = "List of Fruits"
+body = "Hi\n\nI'm sending an attachment with all my fruit."
+message = report_pdf_emailer.generate(sender, receiveer, subject, body, "/tmp/report.pdf")
+report_pdf_emailer.send(message)
